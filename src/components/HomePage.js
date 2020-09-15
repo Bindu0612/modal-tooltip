47aac072d35d2eab69d5c27849525731b0ecee33
Bindu0612 <58510359+Bindu0612@users.noreply.github.com>
2020-09-15 16:14:19 +0530

    new

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,44 +1,55 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import '../App.css';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import {buttonLabel, COVID19ModalHeaderText, COVID19ModalText, COVID19ModalButtonText} from '../constants';
+import { Button, Modal, ModalHeader, ModalBody, Tooltip } from 'reactstrap';
+import { buttonLabel, COVID19ModalHeaderText, COVID19ModalText, COVID19ModalButtonText, COVID19ModalToolTips1, COVID19ModalToolTips2 } from '../constants';
 
-const HomePage = (props)=>{
-   console.log(props)
-    const [modal, setModal] = useState(false);
-    const [tooltipOpen, setTooltipOpen] = useState(false);
+const HomePage = (props) => {
+  console.log(props)
+  const [modal, setModal] = useState(false);
+  const [tooltipOpen, setTooltipOpen] = useState(false);
 
-  const toggle = () =>{
+
+  const toggle = () => {
     setModal(!modal);
-   }
-
-   const toolTip = () => {
-    setTooltipOpen(!tooltipOpen);
-   }
- const data = () => {
-    let nonParsedData = {__html: COVID19ModalText};
-    return nonParsedData;
   }
-        return(
-            <>
-                <div className = "container">
-                <Button color="danger" onClick={toggle}> {buttonLabel} </Button>
-                <Modal isOpen = {modal} toggle = {toggle} >
-                    
-                    <ModalHeader toggle = {toggle} className= "headerClass"> {COVID19ModalHeaderText} </ModalHeader>
-                    
-                    <ModalBody>
-                        
-                        <div dangerouslySetInnerHTML={data()}/>
-                          </ModalBody>
-                    
-                    <ModalFooter>
-                        {COVID19ModalButtonText}
-                    </ModalFooter>
-                </Modal>
-                </div>
-            </>
-        )
+
+  const toolTip = () => {
+    setTooltipOpen(!tooltipOpen);
+  }
+
+  const modalData = () => {
+    let rawModalData = COVID19ModalText;
+    let parsedModalData = document.createElement('textarea');
+    parsedModalData.innerHTML = rawModalData;
+    return { __html: parsedModalData.textContent };
+  }
+
+  const tooltipData = () =>{
+    let rawTootltip = COVID19ModalToolTips1;
+    let parsedTooltip = document.createElement('textarea');    
+    parsedTooltip.innerHTML = rawTootltip;
+    return { __html: parsedTooltip.textContent };
+  }
+
+  return (
+    <>
+      <div className="container">
+        <Button color="danger" onClick={toggle}> {buttonLabel} </Button>
+        <Modal isOpen={modal} toggle={toggle} >
+
+          <ModalHeader toggle={toggle} className="headerClass"> {COVID19ModalHeaderText} </ModalHeader>
+
+          <ModalBody>
+            <div dangerouslySetInnerHTML={modalData()} />
+            <Tooltip placement="right" isOpen={tooltipOpen} target="symptoms" toggle={toolTip}> <div dangerouslySetInnerHTML={tooltipData()} /> </Tooltip>
+            <Button outline color="primary" size="md-bold" onClick={toggle} className = "buttonStyle"> {COVID19ModalButtonText} </Button>
+          </ModalBody>
+        
+        </Modal>
+      
+      </div>
+    </>
+  )
 }
 
 export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom';
-import HomePageDummy from './components/HomePageDummy';
+import HomePage from './components/HomePage';
 import 'bootstrap/dist/css/bootstrap.css';
 // import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <HomePageDummy/>,
+    <HomePage/>,
   document.getElementById('root')
 );
